2af5bb36915b3df32998a23bd1b988f0e78b3d96
Mottaqui Karim <taqqui.karim@gmail.com>
2018-12-19 21:09:11 -0500

    initial commit of a simple server

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Express Server Example
+
+This is a simple express server that demonstrates how interfaces can be leveraged to do useful things behind a web api
+
+## To Install
+
+```
+$ git clone [THIS_REPO]
+$ cd [THIS_REPO]
+$ npm install
+```
+
+## To Run
+
+```
+$ ./node_modules/.bin/nodemon
+```
+
---
+++ index.js
@@ -0,0 +1,55 @@
+const {save, load, add} = require('./user');
+
+// same as:
+// const FileDb = require('./user);
+// const save = FileDb.save
+// const load = FileDb.load
+// const add = FileDb.add
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/ping', (req, res) => {
+    res.json({'message': 'pong'})
+});
+
+app.get('/classlist/add', (req, res) => {
+    
+    const {query} = req;
+    // const query = req.query
+
+    const {name, age} = query;
+    // const name = query.name
+    // const age = query.age
+    // const {name, age} = req.query;
+    console.log(name, age)
+    add({name, age}, (err, data) => {
+        if (err) {
+            res.status(500)
+            res.json({
+                'message': "something went wrong!",
+                "err": err,
+            });
+            return;
+        }
+
+        res.json({
+            "message": "success",
+        });
+    })
+    
+
+});
+
+app.get('/classlist/clear', (req, res) => {
+    res.send('called CLEAR')
+});
+
+app.get('/classlist/load', (req, res) => {
+    res.send('called LOAD')
+})
+
+app.listen(port, () => {
+    console.log(`listening at port ${port}`)
+});
